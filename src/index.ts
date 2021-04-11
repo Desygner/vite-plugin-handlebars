635acd59833994c0d8c053b85414ecbad50874f5
Jared Furlow <jdf2.furlow@gmail.com>
2021-04-11 07:53:39 -0500

    Add .hbs as entry file support

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { compile, registerHelper, RuntimeOptions } from 'handlebars';
 import { resolve } from 'path';
-import { IndexHtmlTransformContext, Plugin as VitePlugin, normalizePath } from 'vite';
+import { IndexHtmlTransformContext, Plugin as VitePlugin, normalizePath, send } from 'vite';
 import { Context, resolveContext } from './context';
 import { registerPartials } from './partials';
+import fs from 'fs';
 
 type CompileArguments = Parameters<typeof compile>;
 type CompileOptions = CompileArguments[1];
@@ -21,7 +22,7 @@ export default function handlebars({
   compileOptions,
   runtimeOptions,
   partialDirectory,
-}: HandlebarsPluginConfig = {}): VitePlugin {
+}: HandlebarsPluginConfig = {}): VitePlugin[] {
   // Keep track of what partials are registered
   const partialsSet = new Set<string>();
 
@@ -31,39 +32,97 @@ export default function handlebars({
     return resolve(root, path);
   });
 
-  return {
-    name: 'handlebars',
+  return [
+    {
+      name: 'handlebars',
 
-    configResolved(config) {
-      root = config.root;
-    },
+      configResolved(config) {
+        root = config.root;
+      },
 
-    async handleHotUpdate({ server, file }) {
-      if (reloadOnPartialChange && partialsSet.has(file)) {
-        server.ws.send({
-          type: 'full-reload',
-        });
+      async handleHotUpdate({ server, file }) {
+        if (reloadOnPartialChange && partialsSet.has(file)) {
+          server.ws.send({
+            type: 'full-reload',
+          });
 
-        return [];
-      }
-    },
-
-    transformIndexHtml: {
-      // Ensure Handlebars runs _before_ any bundling
-      enforce: 'pre',
-
-      async transform(html: string, ctx: IndexHtmlTransformContext): Promise<string> {
-        if (partialDirectory) {
-          await registerPartials(partialDirectory, partialsSet);
+          return [];
         }
+      },
 
-        const template = compile(html, compileOptions);
+      transformIndexHtml: {
+        // Ensure Handlebars runs _before_ any bundling
+        enforce: 'pre',
 
-        const resolvedContext = await resolveContext(context, normalizePath(ctx.path));
-        const result = template(resolvedContext, runtimeOptions);
+        async transform(html: string, ctx: IndexHtmlTransformContext): Promise<string> {
+          if (partialDirectory) {
+            await registerPartials(partialDirectory, partialsSet);
+          }
 
-        return result;
+          const template = compile(html, compileOptions);
+
+          const resolvedContext = await resolveContext(context, normalizePath(ctx.path));
+          const result = template(resolvedContext, runtimeOptions);
+
+          return result;
+        },
       },
     },
-  };
+    {
+      name: 'handlebars:serve',
+      apply: 'serve',
+
+      configureServer(viteDevServer) {
+        // Custom middleware that checks to see if a .hbs file exists for a requested html file
+        // If that .hbs file does exist then it transforms the content using Vite's transformIndexHtml function
+        // and responds with the result
+        // (Wrapped in a function because that tells Vite to add it after the internal middlewares)
+        // A similar middleware is used internally by Vite: https://github.com/vitejs/vite/blob/main/packages/vite/src/node/server/middlewares/indexHtml.ts#L125
+        return () => {
+          viteDevServer.middlewares.use(async (req, res, next) => {
+            const possibleHbsPath = root + req.url?.slice(0, -4) + 'hbs';
+
+            if (
+              req.url?.endsWith('.html') &&
+              req.headers['sec-fetch-dest'] !== 'script' &&
+              fs.existsSync(possibleHbsPath) &&
+              !fs.existsSync(root + req.url)
+            ) {
+              try {
+                let html = fs.readFileSync(possibleHbsPath, 'utf-8');
+                html = await viteDevServer.transformIndexHtml(req.url, html);
+                return send(req, res, html, 'html');
+              } catch (e) {
+                return next(e);
+              }
+            }
+
+            next();
+          });
+        };
+      },
+    },
+    {
+      name: 'handlebars:build',
+      apply: 'build',
+
+      // Tells Rollup/Vite that when an html file that doesn't actually exist in the filesystem,
+      // but a .hbs file with the same name does then consider it a virtual file
+      resolveId(id) {
+        const possibleHbsPath = id.slice(0, -4) + 'hbs';
+
+        if (id.endsWith('.html') && fs.existsSync(possibleHbsPath) && !fs.existsSync(id)) {
+          return id;
+        }
+      },
+      // Allows Rollup to load content from the virtual html files we're telling it exists above
+      load(id) {
+        const possibleHbsPath = id.slice(0, -4) + 'hbs';
+
+        if (id.endsWith('.html') && fs.existsSync(possibleHbsPath) && !fs.existsSync(id)) {
+          return fs.readFileSync(possibleHbsPath, 'utf-8');
+        }
+      },
+    },
+  ];
 }
